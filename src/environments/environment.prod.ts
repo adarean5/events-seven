01e9e0106829997bed13aff342324eec04229f10
Jernej Strazisar <strazisar.jernej@gmail.com>
2021-03-06 15:34:34 +0100

    Add firebase configs for dev and prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+const firebaseConfigProd = {
+    apiKey: "AIzaSyA7GqAfS5mCPuRUjUKZv-MXpMSv8vfCixo",
+    authDomain: "events7-1f4b5.firebaseapp.com",
+    projectId: "events7-1f4b5",
+    storageBucket: "events7-1f4b5.appspot.com",
+    messagingSenderId: "537839600416",
+    appId: "1:537839600416:web:bf8b7a97d67aa4de9c8d45",
+};
+
 export const environment = {
     production: true,
+    firebaseConfig: firebaseConfigProd,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,18 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const firebaseConfigDev = {
+    apiKey: "AIzaSyCZ3seLFt83NTojiiea47QhMbOSBsD-IrQ",
+    authDomain: "events7-dev.firebaseapp.com",
+    projectId: "events7-dev",
+    storageBucket: "events7-dev.appspot.com",
+    messagingSenderId: "882939281004",
+    appId: "1:882939281004:web:862d57b692e9e18bca6249",
+};
+
 export const environment = {
     production: false,
+    firebaseConfig: firebaseConfigDev,
 };
 
 /*
