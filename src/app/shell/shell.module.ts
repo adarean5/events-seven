19ef13a7fb35f5b87be1e0f1cafbafaa328e5715
Jernej Strazisar <strazisar.jernej@gmail.com>
2021-03-10 21:37:54 +0100

    Add a navbar to the shell component

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ShellRoutingModule } from "@/app/shell/shell-routing.module";
 import { ShellComponent } from "./pages/shell/shell.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
     declarations: [ShellComponent],
-    imports: [CommonModule, ShellRoutingModule],
+    imports: [CommonModule, ShellRoutingModule, MatToolbarModule],
 })
 export class ShellModule {}
